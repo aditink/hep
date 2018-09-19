7cf67b5b1bb784ff6ec0be6f12c55c6c601384b3
aditi <ank55@cornell.edu>
2018-09-19 03:17:56 -0400

    Initialising with basic calorimeter

---
+++ B4b-build/CMakeFiles/exampleB4b.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/exampleB4b.dir/exampleB4b.cc.o"
+  "CMakeFiles/exampleB4b.dir/src/B4DetectorConstruction.cc.o"
+  "CMakeFiles/exampleB4b.dir/src/B4PrimaryGeneratorAction.cc.o"
+  "CMakeFiles/exampleB4b.dir/src/B4bActionInitialization.cc.o"
+  "CMakeFiles/exampleB4b.dir/src/B4bEventAction.cc.o"
+  "CMakeFiles/exampleB4b.dir/src/B4bRunAction.cc.o"
+  "CMakeFiles/exampleB4b.dir/src/B4bRunData.cc.o"
+  "CMakeFiles/exampleB4b.dir/src/B4bSteppingAction.cc.o"
+  "exampleB4b.pdb"
+  "exampleB4b"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/exampleB4b.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
